26962c289a292f79d7efbf754eb971d7ab23456d
Dito Manavalan <dito.john@gmail.com>
2020-05-06 17:51:55 -0500

    Added TRACE logging level

--- common/rpimod/stdio/output.py
+++ common/rpimod/stdio/output.py
@@ -201,6 +201,7 @@ def print_warn (msg):
 def print_tip (msg):
     print_color ('magenta', 'TIP: ' + msg)
 
+#################################################################################
 # Logging and debugging
 # Usage:
 # import common.rpimod.stdio.output as coutput
@@ -209,6 +210,58 @@ def print_tip (msg):
 # _FUNC_NAME_ = "function_name"
 # DEBUG_VAR="searchWord"
 # coutput.print_debug(SB_ERR_DEBUG, _FUNC_NAME_, "WATCH: {0} :: {1}".format(DEBUG_VAR, eval(DEBUG_VAR)))
+#################################################################################
+
+def addLoggingLevel(levelName, levelNum, methodName=None):
+    # Reference: https://stackoverflow.com/questions/2183233/how-to-add-a-custom-loglevel-to-pythons-logging-facility
+    """
+    Comprehensively adds a new logging level to the `logging` module and the
+    currently configured logging class.
+
+    `levelName` becomes an attribute of the `logging` module with the value
+    `levelNum`. `methodName` becomes a convenience method for both `logging`
+    itself and the class returned by `logging.getLoggerClass()` (usually just
+    `logging.Logger`). If `methodName` is not specified, `levelName.lower()` is
+    used.
+
+    To avoid accidental clobberings of existing attributes, this method will
+    raise an `AttributeError` if the level name is already an attribute of the
+    `logging` module or if the method name is already present 
+
+    Example
+    -------
+    >>> addLoggingLevel('TRACE', logging.DEBUG - 5)
+    >>> logging.getLogger(__name__).setLevel("TRACE")
+    >>> logging.getLogger(__name__).trace('that worked')
+    >>> logging.trace('so did this')
+    >>> logging.TRACE
+    5
+
+    """
+    if not methodName:
+        methodName = levelName.lower()
+
+    if hasattr(logging, levelName):
+       raise AttributeError('{} already defined in logging module'.format(levelName))
+    if hasattr(logging, methodName):
+       raise AttributeError('{} already defined in logging module'.format(methodName))
+    if hasattr(logging.getLoggerClass(), methodName):
+       raise AttributeError('{} already defined in logger class'.format(methodName))
+
+    # This method was inspired by the answers to Stack Overflow post
+    # http://stackoverflow.com/q/2183233/2988730, especially
+    # http://stackoverflow.com/a/13638084/2988730
+    def logForLevel(self, message, *args, **kwargs):
+        if self.isEnabledFor(levelNum):
+            self._log(levelNum, message, args, **kwargs)
+    def logToRoot(message, *args, **kwargs):
+        logging.log(levelNum, message, *args, **kwargs)
+
+    logging.addLevelName(levelNum, levelName)
+    setattr(logging, levelName, levelNum)
+    setattr(logging.getLoggerClass(), methodName, logForLevel)
+    setattr(logging, methodName, logToRoot)
+
 
 logger = logging.getLogger()
 handler = logging.StreamHandler()
@@ -216,6 +269,12 @@ formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.CRITICAL)
+
+# START: "TRACE" debug level
+TRACE_DEBUG_LEVEL_NUM = logging.DEBUG - 5
+TRACE_DEBUG_LEVEL_NAME = "TRACE"
+addLoggingLevel(TRACE_DEBUG_LEVEL_NAME, TRACE_DEBUG_LEVEL_NUM)
+# END: "TRACE" debug level
 
 def print_debug (debugEnabled, context, msg):
     if debugEnabled:
@@ -226,6 +285,6 @@ def print_debug (debugEnabled, context, msg):
 def print_watcher (debugEnabled, context, expr):
     if debugEnabled:
         frame = sys._getframe(1)
-        logger.setLevel(logging.DEBUG)
-        logger.debug('WATCH [%s] %s :: %s :: %s', context, expr, type(eval(expr, frame.f_globals, frame.f_locals)), repr(eval(expr, frame.f_globals, frame.f_locals)))
+        logger.setLevel(logging.TRACE)
+        logger.trace('[%s] %s :: %s :: %s', context, expr, type(eval(expr, frame.f_globals, frame.f_locals)), repr(eval(expr, frame.f_globals, frame.f_locals)))
         logger.setLevel(logging.CRITICAL)
